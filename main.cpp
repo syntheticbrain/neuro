72ff4c8d947d52c9248de4e7820d112a84a0a5e9
dronov <meierlink23@gmail.com>
2018-05-03 18:59:21 +0300

    .

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int getMatrixSize(string filename) {
     string line;
     ifstream infile;
-    infile.open (filename.c_str());
+    infile.open(filename.c_str());
     getline(infile, line);
     return count(line.begin(), line.end(), '\t') + 1;
 }
@@ -20,14 +20,14 @@ int getMatrixSize(string filename) {
 void read(string filename, boost::numeric::ublas::matrix<int> &A, boost::numeric::ublas::matrix<int> &B) {
     string line;
     ifstream infile;
-    infile.open (filename.c_str());
-    int i=0, j, a;
+    infile.open(filename.c_str());
+    int i = 0, j, a;
 
     while (getline(infile, line) && !line.empty()) {
         istringstream iss(line);
         j = 0;
         while (iss >> a) {
-            A(i,j) = a;
+            A(i, j) = a;
             j++;
         }
         i++;
@@ -38,7 +38,7 @@ void read(string filename, boost::numeric::ublas::matrix<int> &A, boost::numeric
         istringstream iss(line);
         j = 0;
         while (iss >> a) {
-            B(i,j) = a;
+            B(i, j) = a;
             j++;
         }
         i++;
@@ -48,46 +48,74 @@ void read(string filename, boost::numeric::ublas::matrix<int> &A, boost::numeric
 }
 
 void printMatrix(boost::numeric::ublas::matrix<float> matrix) {
-    for (unsigned int i=0; i < matrix.size1(); i++) {
-        for (unsigned int j=0; j < matrix.size2(); j++) {
-            cout << matrix(i, j);
-            if(j+1 != matrix.size2()) {
-                cout << "\t";
-            }
+
+    for (boost::numeric::ublas::matrix<float>::iterator1 it1 = matrix.begin1(); it1 != matrix.end1(); ++it1) {
+        for (boost::numeric::ublas::matrix<float>::iterator2 it2 = it1.begin(); it2 != it1.end(); ++it2) {
+            std::cout << "(" << it2.index1() << "," << it2.index2() << ") = " << *it2 << endl;
         }
         cout << endl;
     }
 }
 
-int main () {
+int activation_function(float x) {
+
+    if (x >= 0.5) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
 
     float vodka = 0.0;
-    float  rain = 1.0;
+    float rain = 1.0;
     float bFriend = 0.0;
 
-    boost::numeric::ublas::matrix<float> INPUTS(1,3);
+    boost::numeric::ublas::matrix<float> INPUTS(1, 3);
 
-    boost::numeric::ublas::matrix<float> HIDDEN_LAYERS(3,2);
+    INPUTS(0, 0) = vodka;
+    INPUTS(0, 1) = rain;
+    INPUTS(0, 2) = bFriend;
 
+    boost::numeric::ublas::matrix<float> WEIGHTS_INPUT_TO_HIDDEN(3, 2);
 
+    WEIGHTS_INPUT_TO_HIDDEN(0, 0) = 0.25;
+    WEIGHTS_INPUT_TO_HIDDEN(0, 1) = 0.5;
+    WEIGHTS_INPUT_TO_HIDDEN(1, 0) = 0.25;
+    WEIGHTS_INPUT_TO_HIDDEN(1, 1) = -0.4;
+    WEIGHTS_INPUT_TO_HIDDEN(2, 0) = 0;
+    WEIGHTS_INPUT_TO_HIDDEN(2, 1) = 0.9;
 
-    INPUTS(0,0) = vodka;
-    INPUTS(0,1) = rain;
-    INPUTS(0,2) = bFriend;
-
-
-    HIDDEN_LAYERS(0,0) = 0.25;
-    HIDDEN_LAYERS(0,1) = 0.5;
-    HIDDEN_LAYERS(1,0) = 0.25;
-    HIDDEN_LAYERS(1,1) = static_cast<float>(-0.4);
-    HIDDEN_LAYERS(2,0) = 0;
-    HIDDEN_LAYERS(2,1) = 0.9;
-
-    boost::numeric::ublas::matrix<float> RESULT = prod(INPUTS, HIDDEN_LAYERS);
+    boost::numeric::ublas::matrix<float> WEIGHTS_OUTPUT_TO_HIDDEN(1, 2);
 
 
+    WEIGHTS_OUTPUT_TO_HIDDEN(0, 0) = -1.0;
+    WEIGHTS_OUTPUT_TO_HIDDEN(0, 1) = 1.0;
 
-    printMatrix(RESULT);
+    boost::numeric::ublas::matrix<float> HIDDEN_OUTPUT(2,1);
+
+
+    boost::numeric::ublas::matrix<float> HIDDEN_INPUT = prod(INPUTS, WEIGHTS_INPUT_TO_HIDDEN);
+
+    vector<float> hiddenOutput;
+
+/*    for (boost::numeric::ublas::matrix<float>::iterator1 it1 = HIDDEN_INPUT.begin1();
+         it1 != HIDDEN_INPUT.end1(); ++it1) {
+        for (boost::numeric::ublas::matrix<float>::iterator2 it2 = it1.begin(); it2 != it1.end(); ++it2) {
+
+            HIDDEN_OUTPUT(0,it2.index2()) = activation_function(*it2);
+        }
+        cout << endl;
+    }
+
+    for(vector<float>::const_iterator it = hiddenOutput.begin(); it != hiddenOutput.end(); ++it) {
+        cout << *it;
+        cout << endl;
+
+    }*/
+
+    printMatrix(HIDDEN_INPUT);
 
     return 0;
 }
